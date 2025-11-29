c4bcc4ff4aa1bf5eb31fe3dd81bce95089bd4e1e
YuliaSalnikova <yuliazhgun@gmail.com>
2025-11-29 16:05:07 +0300

    Правка3 short_id в dsm_sale_data

--- cf_dsm_mart_dsm_sale_data.py
+++ cf_dsm_mart_dsm_sale_data.py
@@ -121,8 +121,8 @@ def cf_dsm_mart_dsm_sale_data():
             logger.info(period)
             logger.info(period.format('YYYY-MM-DD'))
             short_id = uuid.uuid4().hex[:8], 
-            logger.info(short_id)
-            logger.info(f"RUN_ID: triggered_by_{short_id}_{kwargs['dag_run'].run_id}")
+            logger.info(short_id[0])
+            logger.info(f"RUN_ID: triggered_by_{short_id[0]}_{kwargs['dag_run'].run_id}")
             trigger = trigger_dag(
                 dag_id='wf_dsm_mart_dsm_sale_data',
                 run_id=f"triggered_by_{short_id}_{kwargs['dag_run'].run_id}",
