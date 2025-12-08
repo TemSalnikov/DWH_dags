c4f3e8fce37d74d4fb4a32b414a500aa886171ae
NikitaMankov <mankov03@gmail.com>
2025-12-08 08:54:21 +0300

    добавлен парсер, cf и wf даг "Здоровье", ddl "Еаптека"

---
+++ sql_query/ddl_eapteka.sql
@@ -0,0 +1,69 @@
+--drop table stg.mart_fpc_eapteka_report on cluster cluster_2S_2R
+
+create table stg.mart_fpc_eapteka_report on cluster cluster_2S_2R
+(
+	uuid_report text,
+	address text,
+	warehouse_code text,
+	legal_entity text, 
+	inn text,
+	supplier text,
+	product_code text,
+    product_name text,
+    purchase_quantity text,
+    purchase_sum_no_vat text,
+    purchase_sum_with_vat text,
+    sale_quantity text,
+    sale_revenue_purchase_price text,
+    stock_quantity text,
+	name_pharm_chain text,
+    name_report text,
+	start_date text,
+	end_date text,
+	processed_dttm text
+)
+engine = ReplacingMergeTree()
+order by (uuid_report)
+
+
+--drop table kafka.fpc_eapteka_report on cluster cluster_2S_2R
+
+create table kafka.fpc_eapteka_report on cluster cluster_2S_2R
+(
+	uuid_report text,
+	address text,
+	warehouse_code text,
+	legal_entity text, 
+	inn text,
+	supplier text,
+	product_code text,
+    product_name text,
+    purchase_quantity text,
+    purchase_sum_no_vat text,
+    purchase_sum_with_vat text,
+    sale_quantity text,
+    sale_revenue_purchase_price text,
+    stock_quantity text,
+	name_pharm_chain text,
+    name_report text,
+	start_date text,
+	end_date text,
+	processed_dttm text
+)
+engine = Kafka()
+SETTINGS
+    kafka_broker_list = 'kafka1:19092, kafak2:19092, kafka3:19092',
+    kafka_topic_list = 'fpc_eapteka_table_report',
+    kafka_group_name = 'clickhouse',
+    kafka_format = 'JSONColumns',
+    kafka_max_block_size = 10485760,
+    kafka_poll_max_batch_size = 10485760,
+    kafka_handle_error_mode = 'stream',
+    kafka_num_consumers = 1;
+
+
+--drop VIEW kafka.fpc_eapteka_report_mv on cluster cluster_2S_2R
+
+CREATE MATERIALIZED VIEW kafka.fpc_eapteka_report_mv  on cluster cluster_2S_2R TO stg.mart_fpc_eapteka_report AS 
+SELECT * FROM kafka.fpc_eapteka_report;
+
