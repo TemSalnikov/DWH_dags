82f8e1dd4d29af1b77ac250588dc617f01f3c1e6
NikitaMankov <mankov03@gmail.com>
2025-12-18 08:21:56 +0300

    fix parser, add ddl

---
+++ sql_query/ddl_pharmprogress.sql
@@ -0,0 +1,75 @@
+--drop table stg.mart_fpc_pharmprogress_report on cluster cluster_2S_2R
+
+create table stg.mart_fpc_pharmprogress_report on cluster cluster_2S_2R
+(
+	uuid_report text,
+    period text,
+	product_name text,
+	manufacturer text,
+	portfolio text,
+	purchase_quantity text,
+	purchase_sum text,
+	price text,
+	supplier text,
+    remains_quantity text,
+    remains_sum text,
+    pharmacy_name text,
+    pharmacy_category text,
+    legal_entity text,
+    pharmacy_address text,
+	sale_quantity text,
+	sale_sum text,
+    name_report text,
+	name_pharm_chain text,
+	start_date text,
+	end_date text,
+	processed_dttm text
+)
+engine = ReplacingMergeTree()
+order by (uuid_report)
+
+
+--drop table kafka.fpc_pharmprogress_report on cluster cluster_2S_2R
+
+create table kafka.fpc_pharmprogress_report on cluster cluster_2S_2R
+(
+	uuid_report text,
+    period text,
+	product_name text,
+	manufacturer text,
+	portfolio text,
+	purchase_quantity text,
+	purchase_sum text,
+	price text,
+	supplier text,
+    remains_quantity text,
+    remains_sum text,
+    pharmacy_name text,
+    pharmacy_category text,
+    legal_entity text,
+    pharmacy_address text,
+	sale_quantity text,
+	sale_sum text,
+    name_report text,
+	name_pharm_chain text,
+	start_date text,
+	end_date text,
+	processed_dttm text
+)
+engine = Kafka()
+SETTINGS
+    kafka_broker_list = 'kafka1:19092, kafak2:19092, kafka3:19092',
+    kafka_topic_list = 'fpc_pharmprogress_table_report',
+    kafka_group_name = 'clickhouse',
+    kafka_format = 'JSONColumns',
+    kafka_max_block_size = 10485760,
+    kafka_poll_max_batch_size = 10485760,
+    kafka_handle_error_mode = 'stream',
+    kafka_num_consumers = 1;
+
+
+--drop VIEW kafka.fpc_pharmprogress_report_mv on cluster cluster_2S_2R
+
+CREATE MATERIALIZED VIEW kafka.fpc_pharmprogress_report_mv  on cluster cluster_2S_2R TO stg.mart_fpc_pharmprogress_report AS 
+SELECT * FROM kafka.fpc_pharmprogress_report;
+
