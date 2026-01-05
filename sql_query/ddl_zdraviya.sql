9557e5fc34e14ace281592b4fc894a7ba1657e73
NikitaMankov <mankov03@gmail.com>
2026-01-05 14:11:54 +0300

    fix parser

---
+++ sql_query/ddl_zdraviya.sql
@@ -0,0 +1,61 @@
+--drop table stg.mart_fpc_zdraviya_report on cluster cluster_2S_2R
+
+create table stg.mart_fpc_zdraviya_report on cluster cluster_2S_2R
+(
+    uuid_report text,
+    legal_entity text,
+    inn text,
+    pharmacy_address text,
+    product_code text,
+    product_name text,
+    supplier text,
+    purchase_quantity text,
+    sale_quantity text,
+    stock_quantity text,
+    name_report text,
+    name_pharm_chain text,
+    start_date text,
+    end_date text,
+    processed_dttm text
+)
+engine = ReplacingMergeTree()
+order by (uuid_report)
+
+
+--drop table kafka.fpc_zdraviya_report on cluster cluster_2S_2R
+
+create table kafka.fpc_zdraviya_report on cluster cluster_2S_2R
+(
+    uuid_report text,
+    legal_entity text,
+    inn text,
+    pharmacy_address text,
+    product_code text,
+    product_name text,
+    supplier text,
+    purchase_quantity text,
+    sale_quantity text,
+    stock_quantity text,
+    name_report text,
+    name_pharm_chain text,
+    start_date text,
+    end_date text,
+    processed_dttm text
+)
+engine = Kafka()
+SETTINGS
+    kafka_broker_list = 'kafka1:19092, kafak2:19092, kafka3:19092',
+    kafka_topic_list = 'fpc_zdraviya_table_report',
+    kafka_group_name = 'clickhouse',
+    kafka_format = 'JSONColumns',
+    kafka_max_block_size = 10485760,
+    kafka_poll_max_batch_size = 10485760,
+    kafka_handle_error_mode = 'stream',
+    kafka_num_consumers = 1;
+
+
+--drop VIEW kafka.fpc_zdraviya_report_mv on cluster cluster_2S_2R
+
+CREATE MATERIALIZED VIEW kafka.fpc_zdraviya_report_mv  on cluster cluster_2S_2R TO stg.mart_fpc_zdraviya_report AS 
+SELECT * FROM kafka.fpc_zdraviya_report;
+
